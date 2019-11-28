ab2592f6c4b885af8da73fbd054bbbd6adb3b174
Hatice Yetimoglu <haticeytm.ytm@gmail.com>
2019-11-28 20:48:48 +0300

    serve

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-
+var path = require("path");
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
