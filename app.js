254dbe7769395c4c2c602c3a5f792c813c90b89a
Hatice Yetimoglu <haticeytm.ytm@gmail.com>
2019-11-28 20:53:31 +0300

    helloooo

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.redirect('/dist/index.html');
 });
 
 app.listen(3000);
